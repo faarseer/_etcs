78985ee5c53ea661b1a657a045bdb04a20d382be
faarseer <everforeseeing@gmail.com>
2020-05-08 18:54:34 +0900

    0508

--- Scripts/WeekTestScoreToJson.py
+++ Scripts/WeekTestScoreToJson.py
@@ -10,30 +10,71 @@ with open('WeekTest.json') as jsons:
     print(data["WeekTest"])
 
 while True:
-    print("date : (ex:200101) , if input Done, end.")
-    date = input()
-    if date == "Done":
+    print("need date,grade,range change or Done?: True or Done")
+    change = input()
+    if change == "Done":
         break
+    if change == "True":
+        print("date : (ex:200101) , if input Done, end.")
+        date = input()
+        if date == "Done":
+            break
+        print("Grade: , possible range : 중1, 중2")
+        grade = input()
+        print("Range:")
+        _range = input()
+    
     print("\nname : ")
     name = input()
     print("\nscore : ")
     score = input()
     
-    row = {date : {name : score}}
+    row = {
+            date : 
+            [
+                {
+                    "Grade": grade,
+                    "Range": _range,
+                    "Score":
+                    {
+                        name : score
+                    }
+                }
+            ]
+        }
+    print("new data: ")
     print(row)
     
-    if date in data["WeekTest"]:
-        data["WeekTest"][date].update(list(row.values())[0])
+    if date in data["WeekTest"] :
+        isGR = False;
+        for test in data["WeekTest"][date] :
+            if (test["Grade"] == grade) and (test["Range"] == _range) :
+                isGR = True
+                if name in test["Score"] :
+                    print("already exists : ")
+                    print(test)
+                    print("\nreplace True or False?")
+                    replace = input()
+                    if eval(replace) :
+                        test["Score"][name] = score
+                    if not eval(replace) :
+                        test["Score"][name+" 1"] = score
+                else :
+                    test["Score"][name] = score
+        
+        if not isGR:
+            data["WeekTest"][date].append(row[date])            
+
         print("Update :  "+date)
         print(data["WeekTest"][date])
 
     else:
         data["WeekTest"].update(row)
-        print("Make scores at new date : " + date)
+        print("Make test score at new date : " + date)
         print(data["WeekTest"][date])
 
 print("save? True or False")
 save = input()
-if(save == "True"):
+if save == "True":
     with open('WeekTest.json', 'w') as output:
-        json.dump(data, output, ensure_ascii = False)
+        json.dump(data, output, ensure_ascii = False, indent = 4, sort_keys = True)
--- Scripts/mkjsontoMd.py
+++ Scripts/mkjsontoMd.py
@@ -6,13 +6,16 @@ os.chdir("..")
 with open("WeekTest.json") as jsons:
     data = json.load(jsons)
 
-columnKey = ['date', 'name', 'score']
+columnKey = ['Date', 'Grade', 'Range','Name', 'Score']
 
-tablesKey = '|{0}|{1}|{2}|\n|---|---|---|\n'.format(columnKey[0], columnKey[1], columnKey[2])
+tablesKey = '|{0}|{1}|{2}|{3}|{4}|\n|---|---|---|---|---|\n'.format(columnKey[0], columnKey[1], columnKey[2], columnKey[3], columnKey[4])
 
-for _date,val in data["WeekTest"].items():
-    for _kid,_score in val.items():
-        tablesKey += '|{0}|{1}|{2}|\n'.format(_date, _kid,_score)
+for _date,testlist in sorted(data["WeekTest"].items()):
+    for test in testlist:
+        _grade = test["Grade"]
+        _range = test["Range"]
+        for _name,_score in test["Score"].items() :
+            tablesKey += '|{0}|{1}|{2}|{3}|{4}|\n'.format(_date, _grade, _range, _name,_score)
 
 with open('WeekTestScore.md', 'w') as f:
     f.write(tablesKey)
